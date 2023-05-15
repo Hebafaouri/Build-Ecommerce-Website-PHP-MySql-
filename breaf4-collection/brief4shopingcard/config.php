dc4c651cd415dc4ed5002e236f5820e050b17f02
Hebafaouri <126398763+Hebafaouri@users.noreply.github.com>
2023-05-15 11:12:30 +0300

    project update

--- breaf4-collection/brief4shopingcard/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$conn = new mysqli("localhost","root","","jewelry_db");
-	if($conn->connect_error){
-		die("Connection Failed!".$conn->connect_error);
-	}
-?>
--- breaf4-collection/login/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-$id = $_GET['id'];
-$sql = 'DELETE FROM products WHERE id=:id';
-$statement = $connection->prepare($sql);
-if ($statement->execute([':id' => $id])) {
-  header("Location: profile.php");
-}
